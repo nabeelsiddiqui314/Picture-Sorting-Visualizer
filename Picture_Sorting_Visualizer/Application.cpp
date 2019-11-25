a35ff1ba60a9e22d719af076e291bf6f7e2650f1
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2019-11-26 01:14:30 +0530

    added the ability to manage different algorithms.

--- Picture_Sorting_Visualizer/Application.cpp
+++ Picture_Sorting_Visualizer/Application.cpp
@@ -1,12 +1,16 @@
 #include "stdafx.h"
 #include "Application.h"
 
+#include "Shuffle.h"
+
 Application::Application() {
 	m_window.create(sf::VideoMode(800, 600), "Picture Sorter");
 
+	m_array = std::make_shared<PictureArray>();
+
 	applyTextureToImage();
 
-	const auto& pictureSize = m_array.getImageBuffer().getSize();
+	const auto& pictureSize = m_array->getImageBuffer().getSize();
 	const auto& windowSize = m_window.getSize();
 
 	m_pictureQuad.setSize({ (float)pictureSize.x, (float)pictureSize.y });
@@ -20,6 +24,14 @@ void Application::run() {
 
 		m_window.clear();
 
+		if (m_algorithm) {
+			m_algorithm->run();
+
+			if (m_algorithm->hasCompleted()) {
+				m_algorithm.reset();
+			}
+		}
+
 		applyTextureToImage();
 		m_window.draw(m_pictureQuad);
 
@@ -30,12 +42,20 @@ void Application::run() {
 void Application::handleEvents() {
 	sf::Event evnt;
 	while (m_window.pollEvent(evnt)) {
+		if (!m_algorithm) {
+			if (evnt.type == sf::Event::KeyPressed) {
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+					m_algorithm = std::make_unique<Shuffle>(m_array);
+				}
+			}
+		}
+
 		if (evnt.type == sf::Event::Closed) {
 			m_window.close();
 		}
 	}
 }
 void Application::applyTextureToImage() {
-	m_pictureTexture.loadFromImage(m_array.getImageBuffer());
+	m_pictureTexture.loadFromImage(m_array->getImageBuffer());
 	m_pictureQuad.setTexture(&m_pictureTexture);
 }
--- Picture_Sorting_Visualizer/Application.h
+++ Picture_Sorting_Visualizer/Application.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "PictureArray.h"
+#include "Algorithm.h"
 
 class Application
 {
@@ -14,7 +15,9 @@ private:
 private:
 	sf::RenderWindow m_window;
 
-	PictureArray m_array;
+	std::unique_ptr<Algorithm> m_algorithm;
+	std::shared_ptr<PictureArray> m_array;
+
 	sf::Texture m_pictureTexture;
 	sf::RectangleShape m_pictureQuad;
 };
