134155bd6871dc03a242aa72c98cd1c8c5899a61
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2019-11-26 00:35:40 +0530

    removed init pure virtual function.

--- Picture_Sorting_Visualizer/Algorithm.h
+++ Picture_Sorting_Visualizer/Algorithm.h
@@ -7,7 +7,6 @@ public:
 	Algorithm(const std::shared_ptr<PictureArray>& array);
 	virtual ~Algorithm() = default;
 public:
-	virtual void init() = 0;
 	virtual void run() = 0;
 protected:
 	std::shared_ptr<PictureArray> p_array;
